6ac9de5801fea55dddfc199ca5ea3d61771a1a67
Agus Salim <agokz52@gmail.com>
2026-08-22 21:05:18 +0700

    Make fetch function async and streamline response
    Refactor fetch function to be asynchronous and simplify response.

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 export default {
-  fetch(request, env, ctx) {
-    return new Response("SP GUARDIAN WORKER OK", {
-      status: 200,
-      headers: {
-        "content-type": "text/plain; charset=UTF-8"
-      }
-    });
+  async fetch(request, env, ctx) {
+    return new Response("SP GUARDIAN WORKER OK");
   }
 };
